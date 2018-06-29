7b91e6ddbaf2be64d46397c47467b398116cdbc1
shiva hanu <shivaph@hotmail.com>
2018-06-28 21:36:59 -0700

    fixed medical conditions list showing id instead of string terms in user list GET req

--- drf-auth/Users/models.py
+++ drf-auth/Users/models.py
@@ -2,14 +2,13 @@ from django.db import models
 
 
 class MedicalCondition(models.Model):
-    medicalConditionName = models.CharField(max_length=200)
+    medicalConditionName = models.CharField(max_length=200, primary_key=True)
 
     def __str__(self):
         return self.medicalConditionName
 
-
 class User(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, primary_key=True)
     abbrev = models.CharField(max_length=10)
     tktNum = models.CharField(max_length=200)
     mobileNum = models.CharField(max_length=200)
@@ -18,12 +17,16 @@ class User(models.Model):
     isOwner = models.BooleanField(default=False)
     medicalConditions = models.ManyToManyField(MedicalCondition, related_name="MedicalCondition")
 
+    class Meta:
+        unique_together = ('mobileNum', 'abbrev')
+
     def __str__(self):
         """A string representation of the model."""
-        return self.mobileNum + "_" + self.abbrev
+        return self.abbrev
 
 class Group(models.Model):
-    mobileNum = models.CharField(max_length=200)
+    mobileNum = models.CharField(max_length=200, primary_key=True)
+    members = models.ManyToManyField(User, related_name="Group_members")
 
     def __str__(self):
         return self.mobileNum
@@ -31,7 +34,7 @@ class Group(models.Model):
 
 class GroupMembers(models.Model):
     mobileNum = models.CharField(max_length=200)
-    members = models.ManyToManyField(User, related_name="Group_members")
+    members = models.ManyToManyField(User, related_name="GroupMembers")
 
     def __str__(self):
         return self.mobileNum
--- drf-auth/api/urls.py
+++ drf-auth/api/urls.py
@@ -5,11 +5,12 @@ from . import views
 urlpatterns = [
     path('users', views.ListUser.as_view()),
     path('medical-conditions', views.ListMedicalCondition.as_view()),
-    #path('groups', views.ListGroup.as_view()),
+    path('groups', views.ListGroup.as_view()),
 
     path('users/<int:pk>/', views.DetailUser.as_view()),
+    path('groups/<int:pk>/', views.DetailGroup.as_view()),
     #path('^groups/(?P<mobile_num>.+)/$', views.GroupMembersList.as_view()),
-    path('groups', views.GroupMembersList.as_view()),
+    path('group-members', views.GroupMembersList.as_view()),
     path('medical-conditions/<int:pk>/', views.DetailMedicalCondition.as_view()),
 
     path('rest-auth/', include('rest_auth.urls')),
