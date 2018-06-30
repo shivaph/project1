017f2a39498168f35e14e66708e7a12d48aa6685
shiva hanu <shivaph@hotmail.com>
2018-06-30 16:34:41 -0700

    fixed the user retrieve/update/delete view to filter based on mobile_num and abbrev

--- drf-auth/api/urls.py
+++ drf-auth/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 from . import views
 
@@ -7,8 +7,12 @@ urlpatterns = [
     path('medical-conditions', views.ListMedicalCondition.as_view()),
     path('groups', views.ListGroup.as_view()),
 
-    path('users/<int:pk>/', views.DetailUser.as_view()),
-    path('groups/<int:pk>/', views.DetailGroup.as_view()),
+    #path('users/<slug:pk>/', views.DetailUser.as_view()),
+    # note : pk_abbrev is a slug
+    re_path(r'users/mobile_num/(?P<mobileNum>(\d{3})-(\d{3})-(\d{4}))/abbrev/(?P<abbrev>[\w]+)$', views.DetailUser.as_view()),
+    #re_path(r'users/mobile_num/(?P<mobileNum>(\d{3})-(\d{3})-(\d{4}))$', views.DetailUser.as_view()),
+    #re_path(r'users/mobile_num/(?P<pk_mobile_num>[0-9]+)$', views.DetailUser.as_view()),
+    #path('groups/<int:pk>/', views.DetailGroup.as_view()),
     #path('^groups/(?P<mobile_num>.+)/$', views.GroupMembersList.as_view()),
     path('group-members', views.GroupMembersList.as_view()),
     path('medical-conditions/<int:pk>/', views.DetailMedicalCondition.as_view()),
--- drf-auth/api/views.py
+++ drf-auth/api/views.py
@@ -19,17 +19,53 @@ class DetailMedicalCondition(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.MedicalCondition.objects.all()
     serializer_class = serializers.MedicalConditionSerializer
 
-class DetailUser(generics.RetrieveUpdateDestroyAPIView):
-    queryset = models.User.objects.all()
-    serializer_class = serializers.UserSerializer
+#class DetailUser(generics.RetrieveUpdateDestroyAPIView):
+    #queryset = models.User.objects.all()
+    #serializer_class = serializers.UserSerializer
 
 class ListGroup(generics.ListCreateAPIView):
     queryset = models.Group.objects.all()
     serializer_class = serializers.GroupSerializer
 
-class DetailGroup(generics.RetrieveUpdateDestroyAPIView):
-    queryset = models.Group.objects.all()
-    serializer_class = serializers.GroupSerializer
+class DetailUser(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.User.objects.all()
+    serializer_class = serializers.UserSerializer
+    lookup_field = "mobileNum"
+    lookup_url_kwarg = "mobileNum"
+    #lookup_url_kwarg = "pk_mobile_num"
+
+    def get_object(self):
+        #mobile_num = self.kwargs['pk_mobile_num']
+        mobile_num = self.kwargs.get(self.lookup_url_kwarg)
+        abbrev = self.kwargs.get("abbrev")
+        print ("shiva_1:abbrev=" + abbrev)
+        print ("shiva:mobile_num=" + mobile_num)
+        #return models.User.objects.filter(mobileNum=mobile_num)
+
+        #abbrev = self.kwargs['pk_abbrev']
+        #return models.User.objects.filter(mobileNum=mobile_num, abbrev=abbrev)
+
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # Perform the lookup filtering.
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+
+        assert lookup_url_kwarg in self.kwargs, (
+                'Expected view %s to be called with a URL keyword argument '
+                'named "%s". Fix your URL conf, or set the `.lookup_field` '
+                'attribute on the view correctly.' %
+                (self.__class__.__name__, lookup_url_kwarg)
+        )
+
+        #filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
+        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg], "abbrev":abbrev}
+        obj = get_object_or_404(queryset,
+                                **filter_kwargs)  # <-- can see that filtering is performed on the base of 'lookup_field'
+
+        # May raise a permission denied
+        self.check_object_permissions(self.request, obj)
+
+        return obj  # will return the single retrieved object
 
 class GroupMembersList(generics.ListAPIView):
     serializer_class = serializers.GroupMembersSerializer
